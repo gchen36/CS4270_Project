0800884b2f0a10f0a8b203d3a074560d33484570
Natchaphon Ruengsakulrach <nruengsa@gatech.edu>
2014-11-07 14:00:49 -0500

    Create test.py
    This code starts the mininet topology.

---
+++ test.py
@@ -0,0 +1,15 @@
+from mininet.net import Mininet
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
+from mininet.link import TCLink
+from CustomTopo import *
+from mininet.cli import CLI
+
+linkopts1 = {'bw':50, 'delay':'5ms'}
+linkopts2 = {'bw':30, 'delay':'10ms'}
+
+topo = CustomTopo(linkopts1, linkopts2, edge_fanout=3,host_fanout=4)
+
+net = Mininet(topo=topo, link=TCLink)
+net.start()
+CLI(net)
